660081d0d4eff9b60f2e0bcee97b5935cf29b4ed
Pr0curo <jhelbig86@gmail.com>
2019-01-10 21:53:33 +0100

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-pub fn add_one(number:i32) -> i32{
-	number+1
+pub fn add_one(number: i32) -> i32 {
+	number + 1
 }
 
-pub fn add_two(number:i32) -> i32{
-	number+2
+pub fn add_two(number: i32) -> i32 {
+	number + 1 + 1
 }
 
-pub fn add_three(number:i32) -> i32{
-	number+3
+pub fn add_three(number: i32) -> i32 {
+	number + 3
 }
-
